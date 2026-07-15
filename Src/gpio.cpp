3072e0c17ef284a9592b313ccbba0eb135023d28
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-15 15:40:24 +0530

    Update gpio.cpp

--- Src/gpio.cpp
+++ Src/gpio.cpp
@@ -5,26 +5,26 @@ namespace gpio
 
 void Pin::setMode(Mode mode) const
 {
-	setTwoBitField(port_->MODER, pinNumber_ * 2, mode);
+//	setTwoBitField(port_->MODER, pinNumber_ * 2, mode);
 }
 
 void Pin::setOutputType(OutputType outputType) const
 {
-	setBit(port_->OTYPER, pinNumber_, outputType);
+//	setBitValue(port_->OTYPER, pinNumber_, outputType);
 }
 
 void Pin::setOutputSpeed(OutputSpeed outputSpeed) const
 {
-	setTwoBitField(port_->OSPEEDR, pinNumber_ * 2, outputSpeed);
+//	setTwoBitField(port_->OSPEEDR, pinNumber_ * 2, outputSpeed);
 }
 
 void Pin::setPull(Pull pull) const
 {
-	setTwoBitField(port_->PUPDR, pinNumber_ * 2, pull);
+//	setTwoBitField(port_->PUPDR, pinNumber_ * 2, pull);
 }
 void Pin::setAlternateFunction(std::uint8_t AFType) const
 {
-	setFourBitField(port_->AFR[pinNumber_ / 8], (pinNumber_ % 8) * 4, AFType);
+//	setFourBitField(port_->AFR[pinNumber_ / 8], (pinNumber_ % 8) * 4, AFType);
 }
 
 void Pin::configureInput(Pull pull) const
@@ -52,26 +52,40 @@ void Pin::configureAnalog() const
 
 bool Pin::isHigh() const
 {
-
+//	return (read() == PinState::HIGH);
 }
 
 bool Pin::isLow() const
 {
-
+//	return (read() == PinState::LOW);
 }
 
 void Pin::toggle() const
 {
-
+//	if (isLow())
+//	{
+//		write(PinState::HIGH);
+//	}
+//	else
+//	{
+//		write(PinState::LOW);
+//	}
 }
 void Pin::write(PinState state) const
 {
-
+//	if (state == PinState::HIGH)
+//	{
+//		setBit(port_->BSRR, pinNumber_);
+//	}
+//	else
+//	{
+//		setBit(port_->BSRR, pinNumber_ + 16);
+//	}
 }
 
 PinState Pin::read() const
 {
-
+//	return
 }
 
 } // gpio namespace

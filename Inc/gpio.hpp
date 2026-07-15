e1b501532df82e1115e6252efdc416241102ff73
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-15 11:08:23 +0530

    Update gpio.hpp

---
+++ Inc/gpio.hpp
@@ -0,0 +1,43 @@
+#include "stm32f446xx.h"
+#include <cstdint>
+
+namespace gpio
+{
+
+enum class Mode : std::uint8_t
+{
+	INPUT = 0,
+	OUTPUT,
+	ALTERNATE,
+	ANALOG
+};
+
+enum class OutputType : std::uint8_t
+{
+	PUSH_PULL = 0,
+	OPEN_DRAIN
+};
+
+enum class OutputSpeed : std::uint8_t
+{
+	LOW = 0,
+	MEDIUM,
+	FAST,
+	HIGH
+};
+
+enum class Pull : std::uint8_t
+{
+	NONE = 0,
+	UP,
+	DOWN
+};
+
+class Pin
+{
+private:
+	uint8_t pinNumber;
+	GPIO_TypeDef* port;
+};
+
+}	// gpio namespace

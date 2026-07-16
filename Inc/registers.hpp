584874c23280c1b9bcb10aba6415fb65a4c25be7
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-16 13:02:39 +0530

    Update registers.hpp

--- Inc/registers.hpp
+++ Inc/registers.hpp
@@ -2,4 +2,98 @@
 
 #include <cstdint>
 
+namespace reg
+{
 
+// -------------------------------- helper functions ------------------------------------
+
+inline uint32_t singleBitMask(uint8_t bit)
+{
+	return (1U << bit);
+}
+
+inline uint32_t bitMask(uint8_t width)
+{
+	return (1U << width) - 1UL;
+}
+
+inline uint32_t bitMask(uint8_t width, uint8_t shift)
+{
+	return bitMask(width) << shift;
+}
+
+// ------------------------------- register operations ----------------------------------
+
+inline uint32_t read(const volatile uint32_t& reg)
+{
+	return reg;
+}
+
+inline void write(volatile uint32_t& reg, uint32_t value)
+{
+	reg = value;
+}
+
+inline void writeORedValue(volatile uint32_t& reg, uint32_t value)
+{
+	reg |= value;
+}
+
+inline void writeORedValue(volatile uint32_t& reg, uint32_t value, uint8_t shift)
+{
+	reg |= bitMask(value, shift);
+}
+
+inline void reset(volatile uint32_t& reg)
+{
+	reg = 0;
+}
+
+// ------------------------ single bit operations ------------------------
+
+inline void setBit(volatile uint32_t& reg, uint8_t bit)
+{
+	reg |= singleBitMask(bit);
+}
+
+inline void resetBit(volatile uint32_t& reg, uint8_t bit)
+{
+	reg &= ~singleBitMask(bit);
+}
+
+inline bool readBit(const volatile uint32_t& reg, uint8_t bit)
+{
+	return (reg & singleBitMask(bit)) != 0;
+}
+
+// ------------------------- multi bit operations --------------------------
+
+inline void setBits(volatile uint32_t& reg, uint32_t mask)
+{
+	reg |= mask;
+}
+
+inline void setBits(volatile uint32_t& reg, uint8_t width, uint8_t shift)
+{
+	reg |= bitMask(width, shift);
+}
+
+inline void resetBits(volatile uint32_t& reg, uint32_t mask)
+{
+	reg &= ~mask;
+}
+
+inline void resetBits(volatile uint32_t& reg, uint8_t width, uint8_t shift)
+{
+	reg &= ~bitMask(width, shift);
+}
+
+// ------------------------- bit field operations --------------------------
+
+inline void setNBitFieldValue(volatile uint32_t& reg, uint8_t startBit, uint8_t width, uint32_t value)
+{
+	resetBits(reg, width, startBit);
+	writeORedValue(reg, value, startBit);
+}
+
+} // reg namespace

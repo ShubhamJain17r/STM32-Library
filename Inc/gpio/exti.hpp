a18d8aa10fa26028da50469b446415fd13cc55e7
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-18 21:04:35 +0530

    Update exti.hpp

--- Inc/gpio/exti.hpp
+++ Inc/gpio/exti.hpp
@@ -2,3 +2,43 @@
 
 #include "stm32f446xx.h"
 #include <cstdint>
+
+#include "common/callback.hpp"
+#include "gpio/gpio.hpp"
+
+namespace exti {
+
+enum class Edge {
+	RISING,
+	FALLING,
+	BOTH
+};
+
+class ExternalInterrupt : private gpio::Pin {
+private:
+	Edge edge_;
+
+public:
+	constexpr ExternalInterrupt(std::uint8_t pinNumber, GPIO_TypeDef *port, Edge edge) :
+				pinNumber_(pinNumber), port_(port), edge_(edge) {
+		register_instances();
+	}
+
+	void setCallback(InterruptEdge, Callback);
+
+	void handleISR();
+
+private:
+    void enable_nvic();
+
+    void register_instance();
+
+    std::array<Callback, 3> callbacks_{}; // RISING, FALLING, BOTH
+
+public:
+    // Static Dispatcher System
+    static std::array<ExternalInterrupt*, 16> active_instances; // For gpio pin 0-15
+
+};
+
+}

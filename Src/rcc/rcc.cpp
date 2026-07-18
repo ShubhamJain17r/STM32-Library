8509bf56646f93728419184415180f186874a9d1
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-18 20:00:07 +0530

    Update rcc.cpp

--- Src/rcc/rcc.cpp
+++ Src/rcc/rcc.cpp
@@ -6,8 +6,7 @@ namespace rcc {
 
 void enableClock_GPIO(GPIO_TypeDef *port) {
 	// Calculate difference between target port and base GPIOA address
-	std::uint32_t port_index = (reinterpret_cast<std::uint32_t>(port)
-			- GPIOA_BASE) / 0x400;
+	std::uint32_t port_index = (reinterpret_cast<std::uint32_t>(port) - GPIOA_BASE) / 0x400;
 	if (port_index <= 7) { // Validate range (GPIOA to GPIOH)
 		reg::setBit(RCC->AHB1ENR, port_index);
 	}

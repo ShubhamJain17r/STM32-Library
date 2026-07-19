5157d47fecb0cd2d8958ebfe604ecb1bcdc438ca
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-19 20:28:03 +0530

    Update dma.hpp

--- Inc/dma/dma.hpp
+++ Inc/dma/dma.hpp
@@ -3,12 +3,26 @@
 
 namespace dma {
 
+enum class StatusFlag
+{
+	TRANSFER_COMPLETE,
+	HALF_TRANSFER_COMPLETE,
+	TRANSFER_ERROR,
+	DIRECT_MODE_ERROR,
+	FIFO_MODE_ERROR
+};
+
 enum class Channel : std::uint8_t {
     CH0 = 0, CH1, CH2, CH3, CH4, CH5, CH6, CH7
 };
 
 enum class BurstType : std::uint8_t {
     SINGLE = 0, INCR4, INCR8, INCR16
+};
+
+enum class CurrentTarget
+{
+	MEMORY0, MEMORY1
 };
 
 enum class Priority : std::uint8_t {
@@ -19,7 +33,15 @@ enum class DataSize : std::uint8_t {
     BYTE = 0, HALF_WORD, WORD
 };
 
-// Fixed: Unified to UPPER_CASE, corrected spelling ("quarter" vs "forth")
+enum class FifoStatus : std::uint8_t {
+    LESS_THAN_QUARTER     = 0b000, // 0 < fifo_level < 1/4
+    QUARTER_TO_HALF       = 0b001, // 1/4 <= fifo_level < 1/2
+    HALF_TO_THREE_QUARTER = 0b010, // 1/2 <= fifo_level < 3/4
+    THREE_QUARTER_TO_FULL = 0b011, // 3/4 <= fifo_level < full
+    EMPTY                 = 0b100, // FIFO is empty
+    FULL                  = 0b101  // FIFO is full
+};
+
 enum class FifoThreshold : std::uint8_t {
     QUARTER = 0,
     HALF,
@@ -27,7 +49,6 @@ enum class FifoThreshold : std::uint8_t {
     FULL
 };
 
-// Fixed: Unified to UPPER_CASE with clear snake_case separators
 enum class TransferDirection : std::uint8_t {
     PERIPHERAL_TO_MEMORY,
     MEMORY_TO_PERIPHERAL,

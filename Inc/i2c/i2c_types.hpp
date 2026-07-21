7f05cab6a24e2e04c258e5f8ac198daec4feca6d
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-21 15:34:36 +0530

    Create i2c_types.hpp

---
+++ Inc/i2c/i2c_types.hpp
@@ -0,0 +1,15 @@
+/*
+ * i2c_types.hpp
+ *
+ *  Created on: 21-Jul-2026
+ *      Author: shubh
+ */
+
+#ifndef I2C_I2C_TYPES_HPP_
+#define I2C_I2C_TYPES_HPP_
+
+
+
+
+
+#endif /* I2C_I2C_TYPES_HPP_ */

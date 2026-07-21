76f327c4c862d58abca52ec818d893960ac3cbe7
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-21 18:44:33 +0530

    added bus frequencies

--- Inc/rcc/rcc.hpp
+++ Inc/rcc/rcc.hpp
@@ -6,8 +6,17 @@
 
 namespace rcc {
 
-constexpr std::uint32_t SYSCLK = 16000000;
-constexpr std::uint8_t SYSCLK_MHz = 16;
+constexpr std::uint32_t HSI_FREQUENCY  = 16'000'000U;
+constexpr std::uint32_t SYSCLK         = HSI_FREQUENCY;
+constexpr std::uint32_t HCLK           = SYSCLK;
+constexpr std::uint32_t PCLK1          = HCLK;
+constexpr std::uint32_t PCLK2          = HCLK;
+
+constexpr std::uint8_t HSI_MHz         = 16U;
+constexpr std::uint8_t SYSCLK_MHz      = 16U;
+constexpr std::uint8_t HCLK_MHz        = 16U;
+constexpr std::uint8_t PCLK1_MHz       = 16U;
+constexpr std::uint8_t PCLK2_MHz       = 16U;
 
 void enableGpioClock(const GPIO_TypeDef* port);
 void enableDmaClock(const DMA_TypeDef* dmaBase);

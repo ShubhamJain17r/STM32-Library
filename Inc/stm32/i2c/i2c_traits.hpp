a552aca5ac6f60679a64ddad0f2ec490e2f66d2f
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-29 15:37:08 +0530

    Create i2c_traits.hpp

---
+++ Inc/stm32/i2c/i2c_traits.hpp
@@ -0,0 +1,15 @@
+/*
+ * i2c_traits.hpp
+ *
+ *  Created on: 29-Jul-2026
+ *      Author: shubh
+ */
+
+#ifndef STM32_I2C_I2C_TRAITS_HPP_
+#define STM32_I2C_I2C_TRAITS_HPP_
+
+
+
+
+
+#endif /* STM32_I2C_I2C_TRAITS_HPP_ */

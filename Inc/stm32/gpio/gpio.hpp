6c508ded5f59d77816a413b8cf9572d038a67525
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-30 12:00:04 +0530

    added digital I/O classes

--- Inc/stm32/gpio/gpio.hpp
+++ Inc/stm32/gpio/gpio.hpp
@@ -1,15 +1,56 @@
-/*
- * gpio.hpp
- *
- *  Created on: 30-Jul-2026
- *      Author: shubh
- */
+#pragma once
 
-#ifndef STM32_GPIO_GPIO_HPP_
-#define STM32_GPIO_GPIO_HPP_
+#include "stm32f446xx.h"
+#include <cstdint>
 
+#include "gpio/pin_map.hpp"
 
+namespace gpio
+{
 
+class DigitalOutput
+{
+private:
+	struct Pin pin_;
 
+public:
+	DigitalOutput() = delete;
+	~DigitalOutput() = default;
 
-#endif /* STM32_GPIO_GPIO_HPP_ */
+	DigitalOutput(const DigitalOutput&) = delete;
+	DigitalOutput& operator=(const DigitalOutput&) = delete;
+
+	DigitalOutput(DigitalOutput&&) = default;
+	DigitalOutput& operator=(DigitalOutput&&) = default;
+
+	explicit DigitalOutput(Pin pin, DigitalOutputConfig config = {});
+
+private:
+
+public:
+
+};
+
+class DigitalInput
+{
+private:
+	struct Pin pin_;
+
+public:
+	DigitalInput() = delete;
+	~DigitalInput() = default;
+
+	DigitalInput(const DigitalInput&) = delete;
+	DigitalInput& operator=(const DigitalInput&) = delete;
+
+	DigitalInput(DigitalInput&&) = default;
+	DigitalInput& operator=(DigitalInput&&) = default;
+
+	explicit DigitalInput(Pin pin, DigitalInputConfig config = {});
+private:
+
+public:
+
+};
+
+} // namespace gpio

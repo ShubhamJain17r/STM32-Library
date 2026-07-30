4a2604de2d8153c116777aa64a63cc13b5f17dbd
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-30 14:19:06 +0530

    added gpio types and config

--- Inc/stm32/gpio/gpio_types.hpp
+++ Inc/stm32/gpio/gpio_types.hpp
@@ -1,15 +1,65 @@
-/*
- * gpio_types.hpp
- *
- *  Created on: 30-Jul-2026
- *      Author: shubh
- */
+#pragma once
 
-#ifndef STM32_GPIO_GPIO_TYPES_HPP_
-#define STM32_GPIO_GPIO_TYPES_HPP_
+#include "stm32f446xx.h"
+#include <cstdint>
 
+namespace gpio
+{
 
+enum class Mode : std::uint8_t
+{
+	INPUT = 0,
+	OUTPUT,
+	ALTERNATE,
+	ANALOG
+};
 
+enum class OutputType : std::uint8_t
+{
+	PUSH_PULL = 0,
+	OPEN_DRAIN
+};
 
+enum class OutputSpeed : std::uint8_t
+{
+	LOW = 0,
+	MEDIUM,
+	FAST,
+	HIGH
+};
 
-#endif /* STM32_GPIO_GPIO_TYPES_HPP_ */
+enum class Pull : std::uint8_t
+{
+	NONE = 0,
+	UP,
+	DOWN
+};
+
+enum class PinState : std::uint8_t
+{
+	LOW = 0,
+	HIGH
+};
+
+struct DigitalInputConfig
+{
+	Pull pull = Pull::NONE;
+	bool enableInterrupt = false;
+};
+
+struct DigitalOutputConfig
+{
+	OutputType outputType = OutputType::PUSH_PULL;
+	OutputSpeed outputSpeed = OutputSpeed::LOW;
+	Pull pull = Pull::NONE;
+	PinState initialState = PinState::LOW;
+};
+
+struct AlternateConfig
+{
+	OutputType outputType = OutputType::PUSH_PULL;
+	OutputSpeed outputSpeed = OutputSpeed::LOW;
+	Pull pull = Pull::NONE;
+};
+
+} // namespace gpio

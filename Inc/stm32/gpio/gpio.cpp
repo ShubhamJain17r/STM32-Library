d6dcc988886fbfc209fea8144a2cba282169be1d
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-30 20:54:10 +0530

    configured digitalOutput constructor

--- Inc/stm32/gpio/gpio.cpp
+++ Inc/stm32/gpio/gpio.cpp
@@ -8,12 +8,20 @@ namespace gpio
 
 DigitalOutput::DigitalOutput(Pin pin, DigitalOutputConfig config) : pin_(pin)
 {
+	pin_.enableClock();
+
+	setModeOutput();
+	configureOutputType(config.outputType);
+	configureOutputSpeed(config.outputSpeed);
+	configurePull(config.pull);
+
+	write(config.initialState);
 
 }
 
 DigitalInput::DigitalInput(Pin pin, DigitalInputConfig config) : pin_(pin)
 {
-
+	pin_.enableClock();
 }
 
 } // namespace gpio

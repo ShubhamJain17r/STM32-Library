be059d9e3b1e94a2e68db2c85e46d6b42a894730
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-30 20:15:26 +0530

    added digital input output constructor

--- Inc/stm32/gpio/gpio.cpp
+++ Inc/stm32/gpio/gpio.cpp
@@ -1,10 +1,19 @@
-/*
- * gpio.cpp
- *
- *  Created on: 30-Jul-2026
- *      Author: shubh
- */
+#include "stm32f446xx.h"
+#include <cstdint>
 
+#include "stm32/gpio/gpio.hpp"
 
+namespace gpio
+{
 
+DigitalOutput::DigitalOutput(Pin pin, DigitalOutputConfig config) : pin_(pin)
+{
 
+}
+
+DigitalInput::DigitalInput(Pin pin, DigitalInputConfig config) : pin_(pin)
+{
+
+}
+
+} // namespace gpio

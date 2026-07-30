1c64bd4db1e0852a7b215cacfaf24f6809cf81c1
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-30 20:19:42 +0530

    test code for DigitalOutput

--- Src/main.cpp
+++ Src/main.cpp
@@ -5,8 +5,40 @@
 
 using namespace gpio;
 
+inline void delay(std::uint32_t t)
+{
+	for(std::uint32_t i = 0; i < t; ++i);
+}
+
 int main()
 {
 	DigitalOutput led(PA5);
-	while(1);
+
+	while(1)
+	{
+		led.write(PinState::HIGH);
+		delay(1000000);
+		led.write(PinState::LOW);
+		delay(1000000);
+
+		led.high();
+		delay(1000000);
+		led.low();
+		delay(1000000);
+
+		if(led.read() == PinState::LOW)
+		{
+			led.high();
+		}
+
+		delay(2000000);
+
+		if(led.read() == PinState::HIGH)
+		{
+			led.low();
+		}
+
+		delay(6000000);
+
+	}
 }

09083577e956e29533afc6c3a8a425e303bddfec
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-31 07:53:28 +0530

    Create callback.hpp

---
+++ Inc/stm32/common/callback.hpp
@@ -0,0 +1,15 @@
+/*
+ * callback.hpp
+ *
+ *  Created on: 31-Jul-2026
+ *      Author: shubh
+ */
+
+#ifndef STM32_COMMON_CALLBACK_HPP_
+#define STM32_COMMON_CALLBACK_HPP_
+
+
+
+
+
+#endif /* STM32_COMMON_CALLBACK_HPP_ */

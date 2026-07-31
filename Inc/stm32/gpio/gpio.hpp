c6090fe8dea17f7b95810eab14faef3fda8f2f4e
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-31 10:58:33 +0530

    added InterruptInput class

--- Inc/stm32/gpio/gpio.hpp
+++ Inc/stm32/gpio/gpio.hpp
@@ -123,4 +123,45 @@ public:
 
 };
 
+class InterruptInput
+{
+private:
+	Pin pin_;
+
+public:
+	InterruptInput() = delete;
+	~InterruptInput() = default;
+
+	InterruptInput(const InterruptInput&) = delete;
+	InterruptInput& operator=(const InterruptInput&) = delete;
+
+	InterruptInput(InterruptInput&&) = delete;
+	InterruptInput& operator=(InterruptInput&&) = delete;
+
+	explicit InterruptInput(Pin pin, InterruptInputConfig config = {});
+
+private:
+	inline void setModeInput() const noexcept
+	{
+		pin_.port->MODER &= ~(3U << (pin_.number * 2));
+		pin_.port->MODER |=  (static_cast<std::uint8_t>(Mode::INPUT) << (pin_.number * 2));
+	}
+
+	inline void configurePull(Pull pull) const noexcept
+	{
+		pin_.port->PUPDR &= ~(3U << (pin_.number * 2));
+		pin_.port->PUPDR |=  (static_cast<std::uint8_t>(pull) << (pin_.number * 2));
+	}
+
+public:
+	inline PinState read() const noexcept
+	{
+		if(pin_.port->IDR & pin_.mask())
+		{
+			return PinState::HIGH;
+		}
+		return PinState::LOW;
+	}
+};
+
 } // namespace gpio

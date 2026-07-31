229aa49b83b1c6d8b317ced68ccf31014e6d4271
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-31 08:07:54 +0530

    Create registers.hpp

---
+++ Inc/stm32/common/registers.hpp
@@ -0,0 +1,15 @@
+/*
+ * registers.hpp
+ *
+ *  Created on: 31-Jul-2026
+ *      Author: shubh
+ */
+
+#ifndef STM32_COMMON_REGISTERS_HPP_
+#define STM32_COMMON_REGISTERS_HPP_
+
+
+
+
+
+#endif /* STM32_COMMON_REGISTERS_HPP_ */

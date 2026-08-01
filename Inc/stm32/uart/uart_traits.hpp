d42545a7fdcc5f53b9cfd3909bbbcc2a6e7bf3ff
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-01 21:09:19 +0530

    added traits of uart ports

--- Inc/stm32/uart/uart_traits.hpp
+++ Inc/stm32/uart/uart_traits.hpp
@@ -1,15 +1,94 @@
-/*
- * uart_traits.hpp
- *
- *  Created on: 01-Aug-2026
- *      Author: shubh
- */
+#pragma once
 
-#ifndef STM32_UART_UART_TRAITS_HPP_
-#define STM32_UART_UART_TRAITS_HPP_
+#include "stm32f446xx.h"
+#include <cstdint>
+
+#include "stm32/common/rcc.hpp"
+
+namespace uart
+{
+
+template<USART_TypeDef* USART>
+struct Traits;
+
+template<>
+struct Traits<USART1>
+{
+	static constexpr IRQn_Type irq = USART1_IRQn;
+
+	void enableClock() noexcept
+	{
+		rcc::enableUartClock(USART1);
+	}
+
+	static constexpr std::uint32_t busClock = rcc::apb2Clock;
+};
+
+template<>
+struct Traits<USART2>
+{
+	static constexpr IRQn_Type irq = USART2_IRQn;
+
+	void enableClock() noexcept
+	{
+		rcc::enableUartClock(USART2);
+	}
+
+	static constexpr std::uint32_t busClock = rcc::apb1Clock;
+};
+
+template<>
+struct Traits<USART3>
+{
+	static constexpr IRQn_Type irq = USART3_IRQn;
+
+	void enableClock() noexcept
+	{
+		rcc::enableUartClock(USART3);
+	}
+
+	static constexpr std::uint32_t busClock = rcc::apb1Clock;
+};
+
+template<>
+struct Traits<UART4>
+{
+	static constexpr IRQn_Type irq = UART4_IRQn;
+
+	void enableClock() noexcept
+	{
+		rcc::enableUartClock(UART4);
+	}
+
+	static constexpr std::uint32_t busClock = rcc::apb1Clock;
+};
+
+template<>
+struct Traits<UART5>
+{
+	static constexpr IRQn_Type irq = UART5_IRQn;
+
+	void enableClock() noexcept
+	{
+		rcc::enableUartClock(UART5);
+	}
+
+	static constexpr std::uint32_t busClock = rcc::apb1Clock;
+};
+
+template<>
+struct Traits<USART6>
+{
+	static constexpr IRQn_Type irq = USART6_IRQn;
+
+	void enableClock() noexcept
+	{
+		rcc::enableUartClock(USART6);
+	}
+
+	static constexpr std::uint32_t busClock = rcc::apb2Clock;
+};
 
 
 
-
-
-#endif /* STM32_UART_UART_TRAITS_HPP_ */
+} // namespace uart

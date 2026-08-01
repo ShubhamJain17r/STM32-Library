cd550937b7b05bd70f38a2e767757ecb086cb5d1
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-01 21:43:42 +0530

    Create uart_config.hpp

---
+++ Inc/stm32/uart/uart_config.hpp
@@ -0,0 +1,15 @@
+/*
+ * uart_config.hpp
+ *
+ *  Created on: 01-Aug-2026
+ *      Author: shubh
+ */
+
+#ifndef STM32_UART_UART_CONFIG_HPP_
+#define STM32_UART_UART_CONFIG_HPP_
+
+
+
+
+
+#endif /* STM32_UART_UART_CONFIG_HPP_ */

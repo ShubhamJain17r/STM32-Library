58b45f25ce51559ecdb4353bed8cbe1ab8eb29a8
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-01 21:09:05 +0530

    added uart clock enable and clock frequencies

--- Inc/stm32/common/rcc.hpp
+++ Inc/stm32/common/rcc.hpp
@@ -6,6 +6,16 @@
 namespace rcc
 {
 
+inline constexpr std::uint32_t HSI_FREQUENCY = 16'000'000U;
+
+inline constexpr std::uint32_t LSI_FREQUENCY = 32'000U;
+
+inline std::uint32_t systemClock = HSI_FREQUENCY;
+inline std::uint32_t ahbClock    = HSI_FREQUENCY;
+inline std::uint32_t apb1Clock   = HSI_FREQUENCY;
+inline std::uint32_t apb2Clock   = HSI_FREQUENCY;
+
+
 inline void enableGpioClock(GPIO_TypeDef* port) noexcept
 {
 	if(port == GPIOA) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
@@ -21,6 +31,16 @@ inline void enableGpioClock(GPIO_TypeDef* port) noexcept
 inline void enableSyscfgClock() noexcept
 {
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+}
+
+inline void enableUartClock(USART_TypeDef* uart)
+{
+	if(uart == USART1) RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+	if(uart == USART2) RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+	if(uart == USART3) RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
+	if(uart == UART4) RCC->APB1ENR |= RCC_APB1ENR_UART4EN;
+	if(uart == UART5) RCC->APB1ENR |= RCC_APB1ENR_UART5EN;
+	if(uart == USART6) RCC->APB2ENR |= RCC_APB2ENR_USART6EN;
 }
 
 } // namespace rcc

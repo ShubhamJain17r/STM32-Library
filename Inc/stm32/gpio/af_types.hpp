c4245a6b22b9bbc8ea0c153e2e09bc27ba4874f3
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-02 11:22:50 +0530

    renamed file and added alternate function config

---
+++ Inc/stm32/gpio/af_types.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <cstdint>
+
+#include "stm32/gpio/gpio_types.hpp"
+
+namespace gpio
+{
+
+namespace af
+{
+
+enum class AlternateFunction : std::uint8_t
+{
+	AF0, AF1, AF2, AF3, AF4, AF5, AF6, AF7, AF8, AF9, AF10, AF11, AF12, AF13, AF14, AF15
+};
+
+enum class Signal
+{
+	I2C1_SDA, I2C1_SCL,
+	I2C2_SDA, I2C2_SCL,
+	I2C3_SDA, I2C3_SCL,
+
+	SPI1_SCK, SPI1_MOSI, SPI1_MISO, SPI1_NSS,
+	SPI2_SCK, SPI2_MOSI, SPI2_MISO, SPI2_NSS,
+	SPI3_SCK, SPI3_MOSI, SPI3_MISO, SPI3_NSS,
+
+	USART1_TX, USART1_RX,
+	USART2_TX, USART2_RX,
+	USART3_TX, USART3_RX,
+	UART4_TX, UART4_RX,
+	UART5_TX, UART5_RX,
+	USART6_TX, USART6_RX
+};
+
+struct AlternateFunctionConfig
+{
+	OutputType outputType = OutputType::PUSH_PULL;
+	OutputSpeed outputSpeed = OutputSpeed::LOW;
+	Pull pull = Pull::NONE;
+};
+
+} // namespace af
+
+} // namespace gpio

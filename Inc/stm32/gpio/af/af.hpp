2efcc675f80a4858f8facef31582b7c4f6be792d
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-02 14:56:36 +0530

    Create af.hpp

---
+++ Inc/stm32/gpio/af/af.hpp
@@ -0,0 +1,15 @@
+/*
+ * af.hpp
+ *
+ *  Created on: 02-Aug-2026
+ *      Author: shubh
+ */
+
+#ifndef STM32_GPIO_AF_AF_HPP_
+#define STM32_GPIO_AF_AF_HPP_
+
+
+
+
+
+#endif /* STM32_GPIO_AF_AF_HPP_ */

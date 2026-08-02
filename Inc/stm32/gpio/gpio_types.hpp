44aca3847337213ce76f417c11ca60b9254ddac0
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-02 14:46:00 +0530

    removed alternate pin config struct

--- Inc/stm32/gpio/gpio_types.hpp
+++ Inc/stm32/gpio/gpio_types.hpp
@@ -46,12 +46,4 @@ enum class Edge
 	FALLING
 };
 
-// TODO
-struct AlternatePinConfig
-{
-	OutputType outputType = OutputType::PUSH_PULL;
-	OutputSpeed outputSpeed = OutputSpeed::LOW;
-	Pull pull = Pull::NONE;
-};
-
 } // namespace gpio

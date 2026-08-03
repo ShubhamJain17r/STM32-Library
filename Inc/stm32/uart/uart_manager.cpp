2190aacc990d43afe847bcf1b3f137ccc57a8bba
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-03 13:45:54 +0530

    interrupt event handler and callback handler

--- Inc/stm32/uart/uart_manager.cpp
+++ Inc/stm32/uart/uart_manager.cpp
@@ -1,10 +1,73 @@
-/*
- * uart_manager.cpp
- *
- *  Created on: 01-Aug-2026
- *      Author: shubh
- */
+#include "stm32/uart/uart_manager.hpp"
 
+namespace uart::interrupt
+{
 
+static constexpr std::size_t index(Instance I)
+{
+	return static_cast<std::size_t>(I);
+}
 
+inline USART_TypeDef* peripheral(Instance instance)
+{
+    switch(instance)
+    {
+        case Instance::usart1: return USART1;
+        case Instance::usart2: return USART2;
+        case Instance::usart3: return USART3;
+        case Instance::uart4:  return UART4;
+        case Instance::uart5:  return UART5;
+        case Instance::usart6: return USART6;
+    }
 
+    return nullptr;
+}
+
+void UartEvent::setCallback(Instance I, Event intr, Callback cb) noexcept
+{
+	switch(intr)
+	{
+		case Event::TxEmpty:
+			callbacks_[index(I)].txEmpty = cb;
+			break;
+		case Event::TxComplete:
+			callbacks_[index(I)].txComplete = cb;
+			break;
+		case Event::RxNotEmpty:
+			callbacks_[index(I)].rxNotEmpty = cb;
+			break;
+	}
+}
+
+void UartEvent::handleEvent(Instance I) noexcept
+{
+	USART_TypeDef* uart = peripheral(I);
+
+	EventCallbacks& cb = callbacks_[index(I)];
+
+	if((uart->SR & USART_SR_RXNE) && (uart->CR1 & USART_CR1_RXNEIE))
+	{
+		if(cb.rxNotEmpty)
+		{
+			cb.rxNotEmpty();
+		}
+	}
+
+	if((uart->SR & USART_SR_TXE) && (uart->CR1 & USART_CR1_TXEIE))
+	{
+		if(cb.txEmpty)
+		{
+			cb.txEmpty();
+		}
+	}
+
+	if((uart->SR & USART_SR_TC) && (uart->CR1 & USART_CR1_TCIE))
+	{
+		if(cb.txComplete)
+		{
+			cb.txComplete();
+		}
+	}
+}
+
+}
--- Inc/stm32/uart/uart_manager.hpp
+++ Inc/stm32/uart/uart_manager.hpp
@@ -1,15 +1,80 @@
-/*
- * uart_manager.hpp
- *
- *  Created on: 01-Aug-2026
- *      Author: shubh
- */
+#pragma once
 
-#ifndef STM32_UART_UART_MANAGER_HPP_
-#define STM32_UART_UART_MANAGER_HPP_
+#include "stm32f446xx.h"
+#include <cstdint>
 
+#include "stm32/common/callback.hpp"
 
+#include "stm32/uart/uart_traits.hpp"
 
+namespace uart::interrupt
+{
 
+enum class Event : std::uint8_t
+{
+	TxEmpty,
+	TxComplete,
+	RxNotEmpty
+};
 
-#endif /* STM32_UART_UART_MANAGER_HPP_ */
+inline void enableEvent(USART_TypeDef* uart, Event intr) noexcept
+{
+    switch(intr)
+    {
+        case Event::TxEmpty:
+            uart->CR1 |= USART_CR1_TXEIE;
+            break;
+
+        case Event::TxComplete:
+            uart->CR1 |= USART_CR1_TCIE;
+            break;
+
+        case Event::RxNotEmpty:
+            uart->CR1 |= USART_CR1_RXNEIE;
+            break;
+    }
+}
+
+inline void disableEvent(USART_TypeDef* uart, Event intr) noexcept
+{
+    switch(intr)
+    {
+        case Event::TxEmpty:
+            uart->CR1 &= ~USART_CR1_TXEIE;
+            break;
+
+        case Event::TxComplete:
+            uart->CR1 &= ~USART_CR1_TCIE;
+            break;
+
+        case Event::RxNotEmpty:
+            uart->CR1 &= ~USART_CR1_RXNEIE;
+            break;
+    }
+}
+
+template<Instance I>
+inline void enableIRQ() noexcept
+{
+	NVIC_EnableIRQ(Traits<I>::irq);
+}
+
+struct EventCallbacks
+{
+	Callback txEmpty = nullptr;
+	Callback txComplete = nullptr;
+	Callback rxNotEmpty = nullptr;
+};
+
+class UartEvent
+{
+public:
+	static void setCallback(Instance, Event, Callback) noexcept;
+
+	static void handleEvent(Instance) noexcept;
+
+private:
+	inline static EventCallbacks callbacks_[6];
+};
+
+} // namespace uart::interrupt

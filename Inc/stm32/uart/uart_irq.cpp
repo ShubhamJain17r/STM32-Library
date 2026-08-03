24d9b8e85794d611f620247aff8149edacd8ab8c
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-03 13:46:05 +0530

    added uart ISR

--- Inc/stm32/uart/uart_irq.cpp
+++ Inc/stm32/uart/uart_irq.cpp
@@ -1,10 +1,36 @@
-/*
- * uart_irq.cpp
- *
- *  Created on: 01-Aug-2026
- *      Author: shubh
- */
+#include "stm32/uart/uart_manager.hpp"
 
+extern "C"
+{
 
+void USART1_IRQHandler()
+{
+	uart::interrupt::UartEvent::handleEvent(uart::Instance::usart1);
+}
 
+void USART2_IRQHandler()
+{
+	uart::interrupt::UartEvent::handleEvent(uart::Instance::usart2);
+}
 
+void USART3_IRQHandler()
+{
+	uart::interrupt::UartEvent::handleEvent(uart::Instance::usart3);
+}
+
+void UART4_IRQHandler()
+{
+	uart::interrupt::UartEvent::handleEvent(uart::Instance::uart4);
+}
+
+void UART5_IRQHandler()
+{
+	uart::interrupt::UartEvent::handleEvent(uart::Instance::uart5);
+}
+
+void USART6_IRQHandler()
+{
+	uart::interrupt::UartEvent::handleEvent(uart::Instance::usart6);
+}
+
+} // extern "C"

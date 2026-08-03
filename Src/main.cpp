dc766d1912f0dad290b215e1d97a87d818a25dd4
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-03 13:46:18 +0530

    testing uart seril communication

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,49 +1,86 @@
 #include "stm32f446xx.h"
 #include <cstdint>
-
-#include "stm32/gpio/gpio.hpp"
-
-using namespace gpio;
-
+//
+//#include "stm32/gpio/gpio.hpp"
+//
+//using namespace gpio;
+//
 inline void delay(std::uint32_t t)
 {
 	for(std::uint32_t i = 0; i < t; ++i);
 }
+//
+//int main()
+//{
+//	DigitalOutput led(PA5,
+//	{
+//			.outputType = OutputType::PUSH_PULL,
+////			.outputSpeed = OutputSpeed::LOW,
+//			.pull = Pull::DOWN,
+////			.initialState = PinState::HIGH
+//	});
+//
+//	while(1)
+//	{
+//		led.write(PinState::HIGH);
+//		delay(1000000);
+//		led.write(PinState::LOW);
+//		delay(1000000);
+//
+//		led.high();
+//		delay(1000000);
+//		led.low();
+//		delay(1000000);
+//
+//		if(led.read() == PinState::LOW)
+//		{
+//			led.high();
+//		}
+//
+//		delay(2000000);
+//
+//		if(led.read() == PinState::HIGH)
+//		{
+//			led.low();
+//		}
+//
+//		delay(6000000);
+//	}
+//}
 
+#include "stm32/uart/uart.hpp"
+#include "stm32/gpio/gpio.hpp"
+
+using namespace uart;
+using namespace gpio;
 int main()
 {
-	DigitalOutput led(PA5,
-	{
-			.outputType = OutputType::PUSH_PULL,
-//			.outputSpeed = OutputSpeed::LOW,
-			.pull = Pull::DOWN,
-//			.initialState = PinState::HIGH
-	});
+	Uart2 Serial;
+//
+//	DigitalOutput led(PA5);
+//
+//	while(1)
+//	{
+//		Serial.write("Hello World\r\n");
+//		delay(1000000);
+//
+//		if(Serial.available())
+//		{
+//			char c = Serial.read();
+//
+//			Serial.write(c);
+//
+//			if(c == 'n')
+//			{
+//				led.high();
+//			}
+//		}
+//	}
 
-	while(1)
-	{
-		led.write(PinState::HIGH);
-		delay(1000000);
-		led.write(PinState::LOW);
-		delay(1000000);
+	char c = Serial.read();
 
-		led.high();
-		delay(1000000);
-		led.low();
-		delay(1000000);
+	Serial.write(c);
 
-		if(led.read() == PinState::LOW)
-		{
-			led.high();
-		}
+	Serial.write("Hello World\r\n");
 
-		delay(2000000);
-
-		if(led.read() == PinState::HIGH)
-		{
-			led.low();
-		}
-
-		delay(6000000);
-	}
 }

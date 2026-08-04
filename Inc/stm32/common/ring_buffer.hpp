c9085d2fbd0803b737e89846a85c088a5f36fcaa
Shubham Jain <shubhamjain00050@gmail.com>
2026-08-04 11:57:45 +0530

    declared ring buffer

--- Inc/stm32/common/ring_buffer.hpp
+++ Inc/stm32/common/ring_buffer.hpp
@@ -1,15 +1,40 @@
-/*
- * ring_buffer.hpp
- *
- *  Created on: 03-Aug-2026
- *      Author: shubh
- */
+#pragma once
 
-#ifndef STM32_COMMON_RING_BUFFER_HPP_
-#define STM32_COMMON_RING_BUFFER_HPP_
+#include <cstdint>
 
+namespace buffer
+{
 
+template<typename T, std::size_t Capacity>
+class RingBuffer
+{
+public:
 
+    bool push(const T&) noexcept;
 
+    bool pop(T&) noexcept;
 
-#endif /* STM32_COMMON_RING_BUFFER_HPP_ */
+    bool empty() const noexcept;
+
+    bool full() const noexcept;
+
+    std::size_t size() const noexcept;
+
+    constexpr std::size_t capacity() const noexcept;
+
+    std::size_t freeSpace() const noexcept;
+
+    void clear() noexcept;
+
+private:
+
+    T buffer_[Capacity];
+
+    std::size_t head_;
+
+    std::size_t tail_;
+
+    std::size_t count_;
+};
+
+} // namesapce buffer
